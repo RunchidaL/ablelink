79036c4c2adb5dd909ec5a3746535282d2367999
RunchidaL <runchida.o@mail.kmutt.ac.th>
2022-06-13 18:24:24 +0700

    download

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -10,12 +10,42 @@
 </div>
 
 <div class="container-fluid">
-<div class="row">
-    <div class="col">
-        <button><i class="bi bi-file-text"></i><br><p>Catelog</p></button>
-        <button><i class="bi bi-file-slides"></i></i><br><p>Presentation</p></button>
-        <button><i class="bi bi-camera-video"></i><br><p>VDO</p></button>
+    <div class="row">
+        <div class="col">
+            <button><i class="bi bi-file-text"></i><br><p>Catelog</p></button>
+            <button><i class="bi bi-file-slides"></i></i><br><p>Presentation</p></button>
+            <button><i class="bi bi-camera-video"></i><br><p>VDO</p></button>
+        </div>
     </div>
-</div>
+    <div class="section">
+        <div class="title">
+            <p>Catelog</p>
+        </div>
+        <div class="desc">
+            <p>Catelog ของบริษัท</p>
+            <p>Catelog ของบริษัทอื่น</p>
+            <p>Catelog ของบริษัทอื่น</p>
+        </div>
+    </div>
+    <div class="section">
+        <div class="title">
+            <p>Presentation</p>
+        </div>
+        <div class="desc">
+            <p>Presentation ของ____</p>
+            <p>Presentation ของ____</p>
+            <p>Presentation ของ____</p>
+        </div>
+    </div>
+    <div class="section">
+        <div class="title">
+            <p>VDO</p>
+        </div>
+        <div class="desc">
+            <p>VDO ของ____</p>
+            <p>VDO ของ____</p>
+            <p>VDO ของ____</p>
+        </div>
+    </div>
 </div>
 @endsection
